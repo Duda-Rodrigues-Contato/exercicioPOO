29ae9d454196387a4ee2fb14d178d7ca52145d9b
Duda-Rodrigues-Contato <mdudarodrigues.contato@gmail.com>
2025-11-19 15:15:57 -0300

    Inicializando a questoa 6 da lista 3.

---
+++ lista3POO/src/questao6.java
@@ -0,0 +1,43 @@
+public class questao6 {
+
+}
+
+class Produto {
+
+    private int codigo;
+    private String nome;
+    private double preco;
+
+    public Produto(int codigo, String nome, double preco) {
+
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+}
